c792650f1a378b00eabb5bcbd138a297c728650c
DTHutton <48737212+DTHutton@users.noreply.github.com>
2019-07-19 14:02:30 -0400

    still working on moment.js functions

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,27 +1,54 @@
 $(document).ready(function () {
 
+    // pushes user input to schedule table
     $("#submit").on("click", function (event) {
         event.preventDefault();
 
         let userTrain = $("#newTrainInfo").val().trim();
         let userDest = $("#newDestinationInfo").val().trim();
-        let userTime = $("#newStartTime").val().trim();
         let userFreq = $("#newTrainFrequency").val().trim();
 
         console.log(userTrain);
         console.log(userDest);
-        console.log(userTime);
         console.log(userFreq);
+
+        let userArrival = nextArrival();
+        let userMinutes = minutesAway();
 
         let userInput = `
         <tr>
             <td>${userTrain}</td>
             <td>${userDest}</td>
             <td>${userFreq}</td>
-            <td>placeholder</td>
-            <td>placeholder</td>
+            <td>${userArrival}</td>
+            <td>${userMinutes}</td>
         </tr>
         `
+
         $("#newTrain").prepend(userInput);
     });
+
+    //calculates next arrival
+    let nextArrival = () => {
+
+        let userTime = $("#newStartTime").val().trim();
+
+        let userTimeVal = moment(userTime, "HHmm").format("HH:mm");
+
+        let currentTimeVal = moment().format("HH:mm");
+
+        console.log(userTimeVal.diff(moment(currentTimeVal, "HHmm"), "minutes"));
+        
+
+    }
+
+    //calculates minutes away
+    let minutesAway = () => {
+
+        console.log(moment().format('LT'))
+
+    }
+
+
+
 });
