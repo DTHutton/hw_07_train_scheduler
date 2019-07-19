6ad5abefa516cb274457eae7fb0c39f98c0b701c
DTHutton <48737212+DTHutton@users.noreply.github.com>
2019-07-19 11:17:36 -0400

    onclick function pushing user input to table is working

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,9 +1,27 @@
 $(document).ready(function () {
 
-    console.log("main.js test");
+    $("#submit").on("click", function (event) {
+        event.preventDefault();
 
-    console.log(moment());
+        let userTrain = $("#newTrainInfo").val().trim();
+        let userDest = $("#newDestinationInfo").val().trim();
+        let userTime = $("#newStartTime").val().trim();
+        let userFreq = $("#newTrainFrequency").val().trim();
 
-    $(".test2").text("jquery test")
+        console.log(userTrain);
+        console.log(userDest);
+        console.log(userTime);
+        console.log(userFreq);
 
+        let userInput = `
+        <tr>
+            <td>${userTrain}</td>
+            <td>${userDest}</td>
+            <td>${userFreq}</td>
+            <td>placeholder</td>
+            <td>placeholder</td>
+        </tr>
+        `
+        $("#newTrain").prepend(userInput);
+    });
 });
